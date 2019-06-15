c4a483b0620a9dd0c437a07d3055732b3aa421dc
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-15 14:02:25 -0500

    Added client side folder/added react dependencies

---
+++ client/src/user-pages/DadJokes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class DadJokes extends React.Component{
+render(){
+    return(
+        <>
+            <h3>Here are some dad jokes:</h3>
+        </>
+    )
+}
+}
+
+export default DadJokes
---
+++ client/src/user-pages/SignIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SignIn extends React.Component{
+render(){
+    return(
+        <>
+            <h3>Sign In</h3>
+        </>
+    )
+}
+}
+
+export default SignIn
